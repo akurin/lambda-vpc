dcb91bed4b9bf6733173e2083c2ca76617c7d1c4
Alexey Kurin <alexey.kurin.nik@gmail.com>
2021-08-06 19:41:31 +0300

    Add VPC

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,15 +1,20 @@
 import * as cdk from "@aws-cdk/core";
+import { Duration } from "@aws-cdk/core";
 import {
   LambdaIntegration,
   MethodLoggingLevel,
   RestApi,
 } from "@aws-cdk/aws-apigateway";
 import { AssetCode, Function, Runtime } from "@aws-cdk/aws-lambda";
-import { Duration } from "@aws-cdk/core";
+import { SubnetType, Vpc } from "@aws-cdk/aws-ec2";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = new Vpc(this, "TheVPC", {
+      cidr: "10.0.0.0/16",
+    });
 
     const restApi = new RestApi(this, this.stackName + "RestApi", {
       deployOptions: {
@@ -27,6 +32,10 @@ export class CdkStack extends cdk.Stack {
       code: new AssetCode("../hello-lambda/built/"),
       memorySize: 256,
       timeout: Duration.seconds(10),
+      vpc: vpc,
+      vpcSubnets: {
+        subnetType: SubnetType.PRIVATE,
+      },
     });
 
     restApi.root.addMethod("GET", new LambdaIntegration(lambdaFunction, {}));
